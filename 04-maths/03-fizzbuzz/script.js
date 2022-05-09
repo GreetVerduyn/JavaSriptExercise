12c11372c345a016979a48aa11b140e5eb203e42
Greet Verduyn <greetverduyn@proximus.be>
2022-05-09 16:20:50 +0200

    exc 04-03 done

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -11,6 +11,20 @@
 
 (function() {
 
-    // your code here
+           for(var i=1; i<100; i++) {
+            let res = i;
+            if ((i % 3) === 0 && (i % 5) === 0) {
+                res += ' fizzbuzz';
+            }
+            else if ((i % 5) === 0) {
+                res += ' buzz';
+            }
+            else if ((i % 3) === 0) {
+                res += ' fizz';
+            }
+
+            console.log(`${res}`);
+        }
+
 
 })();
